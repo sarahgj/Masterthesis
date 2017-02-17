26bf3057ebcaeaa342708ebe73148ea1b2dbfce9
Sarah Gjermo <sarahgj@users.noreply.github.uio.no>
2017-02-17 18:01:39 +0100

    Add files via upload

---
+++ make_files_for_Flexpart_M91.m
@@ -0,0 +1,312 @@
+% Reads input data from a xlsx file in form of dates, times, lat, lon and
+% emission fluxes. 
+% Write release files to be used for FLEXPART
+% Check if its over ground or over sea level
+% Got it from Alina Fiehn
+clear all;
+close all;
+clc
+%run = 5;
+
+%Choose halocarbon
+type = 'methyliodide';
+%type = 'dibromomethane';
+%type = 'bromoform';
+
+%Setting end date
+if strcmp(type, 'methyliodide')
+    %end_simulation_date = '20130103 000000';
+    simulation_days = 10;
+    %Set number of simulation
+    simulation = 'F10';
+elseif strcmp(type, 'bromoform')
+    %end_simulation_date = '20130123 000000';
+    simulation_days = 92;
+    %Set number of simulation
+    simulation = 'F92';
+elseif strcmp(type, 'dibromomethane')
+    %end_simulation_date = '20131031 000000';
+    simulation_days = 548;
+    %Set number of simulation
+    simulation = 'F548';
+end
+
+%% read variables from file
+infile = xlsread('/uio/hume/student-u17/sarahgj/Master/Data/VSLS_measurements/M91_corrected_fluxes_Alina');
+
+%reading the fluxes and setting the right molare masse
+if strcmp(type, 'methyliodide')
+    emissions = infile(:,20); % [pmol/(m²*hr)]
+    molm = 0.14194;           % Molare Masse [kg/mol]
+elseif strcmp(type, 'dibromomethane')
+    emissions = infile(:,21); % [pmol/(m²*hr)]
+    molm = 0.17383;           % Molare Masse [kg/mol]    
+elseif strcmp(type, 'bromoform')
+    emissions = infile(:,22); % [pmol/(m²*hr)]
+    molm = 0.25273;           % Molare Masse [kg/mol] 
+else
+    fprintf('Choose type of halocarbon on line 12-14')
+end
+
+%Molar masses from Alina
+%molm = 0.2527;   % Molare Masse Bromoform
+%molm = 0.14195;  % Molare Masse Methyliodide
+%molm  = 0.06214; % Molare Masse DMS [kg/mol]
+ 
+%SKIP NaNs
+ind=find(~isnan(emissions)); % indices of fluxes
+len = length(ind);
+flux = emissions(ind);   % [pmol/(m²*hr)]
+
+%other variables
+day = infile(ind,3);
+hour = infile(ind,4);
+lat = infile(ind,7);
+lon = infile(ind,8);
+
+
+%% producing two strings with the date and time window for the release
+measuredates = datenum(2012, 12, day, hour, 0, 0);
+startdates   = measuredates - 1./48.; %halfanhour
+enddates     = measuredates + 1./48.; %halfanhour
+end_simulation_dates = measuredates + simulation_days;
+startstring  = datestr(startdates,30);
+endstring    = datestr(enddates,30);
+end_simulation_string = datestr(end_simulation_dates,30);
+
+
+% correcting lon for positive values to the West
+% lon = -lon;
+
+% producing lat and lon windows for the release
+lat1 = lat - 0.0001;
+lat2 = lat + 0.0001;
+lon1 = lon - 0.0001;
+lon2 = lon + 0.0001;
+
+
+%% Calculating emitted masses M
+
+%constants to be used
+pico = 1e-12;
+micro = 1e-6;
+dlat = 22.18; %distance for 0.0002 degrees in latitude [m]
+Na = 6.0221415e23; %Avogadros constant, number of particles in one mol 
+
+area = NaN(len,1);
+particles = NaN(len,1);
+partrelease = NaN(len,1);
+mass = NaN(len,1);
+for i = 1:len
+    cp = cos(lat(i)*3.1415926/180.);
+    sp = sin(lat(i)*3.1415926/180.);
+    a = 6378137.0; %equatorial earth radii
+    b = 6356752.3; %polar earth radii
+    % dlondeg is the distance for 1 degree in longitude [m]
+    % dlon is the distance for 0.0002 degrees in longitude [m]
+    dlondeg = 3.1415926/180. * cp * sqrt( (a^4 * cp^2 + b^4 * sp^2) / ((a*cp)^2 + (b*sp)^2) );
+    dlon = dlondeg*2./10000.;
+    area(i) = dlat * dlon; % release windows: 0.0002 lat * 0.0002 lon [m2]
+    % at entspricht der Anzahl von Teilchen emittiert über der Fläche A in einer Stunde als Funktion des Flusses F
+    %particles(i) = round(flux(i) * area(i) * pico * Na); %[particles/hr]*1hr = total number of particles released
+    %partrelease(i) = round(particles(i)*1e-13); % why *1e-13?
+    %if (partrelease(i) < 0) 
+    %   partrelease(i) = 0;
+    %end
+    mass(i) = flux(i) * area(i) * pico * molm; % [kg/hr]*1hr = total mass released
+    % partikelanzahl wird auf konstante Werte gesetzt
+    partrelease(i) = 10000;
+end
+
+positive_index = find(mass>=0);
+%open file to write released mass
+if strcmp(type, 'methyliodide')
+   dlmwrite('/uio/hume/student-u17/sarahgj/Master/Data/VSLS_measurements/released_methyliodide_M91.dat',mass(positive_index), '\t') 
+
+elseif strcmp(type, 'dibromomethane')
+   dlmwrite('/uio/hume/student-u17/sarahgj/Master/Data/VSLS_measurements/released_dibromomethane_M91.dat',mass(positive_index), '\t') 
+
+elseif strcmp(type, 'bromoform')
+   dlmwrite('/uio/hume/student-u17/sarahgj/Master/Data/VSLS_measurements/released_bromoform_M91.dat',mass(positive_index), '\t') 
+end
+
+%% Writing RELEASE files
+j = 0;
+for i = 1:len
+    if (mass(i) > 0)     
+        j = j+1;    
+        istr = int2str(j); 
+        
+        %open file to write
+        if strcmp(type, 'methyliodide')
+           fid = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Methyliodide/RELEASES-' istr], 'wt');
+        elseif strcmp(type, 'dibromomethane')
+           fid = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Dibromomethane/RELEASES-' istr], 'wt');
+        elseif strcmp(type, 'bromoform')
+           fid = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Bromoform/RELEASES-' istr], 'wt');
+        else
+            fprintf('Thats wierd :O')
+        end
+        
+        %WRITE RELEASES
+        fprintf(fid, '*************************************************************************\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*   Input file for the Lagrangian particle dispersion model FLEXPART    *\n');
+        fprintf(fid, '*                        Please select your options                     *\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*                                                                       *\n');
+        fprintf(fid, '*************************************************************************\n');
+        fprintf(fid, '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n');
+        fprintf(fid, '  1                                                                      \n');      
+        fprintf(fid, '___                        i3    Total number of species emitted        \n'); 
+        fprintf(fid, '                                                                         \n');
+        if strcmp(type, 'methyliodide')
+            fprintf(fid, '  7                                                                      \n');      
+        elseif strcmp(type, 'dibromomethane')
+            fprintf(fid, '  1                                                                      \n');      
+        elseif strcmp(type, 'bromoform')
+            fprintf(fid, '  2                                                                      \n');      
+        end    
+        fprintf(fid, '___                        i3    Index of species in file SPECIES        \n'); 
+        fprintf(fid, '                                                                         \n');
+        fprintf(fid, '=========================================================================\n');
+        fprintf(fid, '%s %s                  \n', startstring(i,1:8), startstring(i,10:15));
+        fprintf(fid, '________ ______           i8,1x,i6 Beginning date and time of release \n');
+        fprintf(fid, '                                                                         \n');  
+        fprintf(fid, '%s %s                  \n', endstring(i,1:8), endstring(i,10:15));
+        fprintf(fid, '________ ______           i8,1x,i6 Ending date and time of release    \n');
+        fprintf(fid, '                                                                         \n'); 
+        fprintf(fid, '%9.4f                       \n', lon1(i));
+        fprintf(fid, '____.____                 f9.4  Longitude [DEG] of lower left corner     \n');
+        fprintf(fid, '                                                                         \n');  
+        fprintf(fid, '%9.4f                       \n', lat1(i));
+        fprintf(fid, '____.____                 f9.4  Latitude [DEG] of lower left corner      \n');
+        fprintf(fid, '                                                                         \n'); 
+        fprintf(fid, '%9.4f                      \n', lon2(i));
+        fprintf(fid, '____.____                 f9.4  Longitude [DEG] of lower left corner     \n');
+        fprintf(fid, '                                                                         \n'); 
+        fprintf(fid, '%9.4f                      \n', lat2(i));
+        fprintf(fid, '____.____                 f9.4  Latitude [DEG] of upper right corner     \n');
+        fprintf(fid, '                                                                         \n');       
+        fprintf(fid, '        1                                                                \n');
+        fprintf(fid, '_________                 i9    1 for m above ground, 2 for m above sea level  \n');
+        fprintf(fid, '                                                                         \n');    
+        fprintf(fid, '    0.000                                                                \n');
+        fprintf(fid, '_____.___                 f10.3 Lower z-level (in m agl or m asl)        \n'); 
+        fprintf(fid, '                                                                         \n');     
+        fprintf(fid, '    0.000                                                                \n');
+        fprintf(fid, '_____.___                 f10.3 Upper z-level (in m agl or m asl)        \n');
+        fprintf(fid, '                                                                         \n'); 
+        fprintf(fid, '%9i                        \n', partrelease(i));
+        fprintf(fid, '_________                 i9    Total number of particles to be released \n');
+        fprintf(fid, '                                                                         \n');
+        fprintf(fid, '%9.4e                                        \n', mass(i));
+        fprintf(fid, '_.____E__                e9.4  Total mass emitted                        \n');
+        fprintf(fid, '                                                                         \n');
+        %   fprintf(fid, '%9.4e                  e9.4  Total mass emitted                      \n', null);
+        %   fprintf(fid, '                                                                         \n');
+        %   fprintf(fid, '                                                                         \n'); 
+        fprintf(fid, 'M91cruice_01-26_Dec_V%2i                                                 \n', i);
+        fprintf(fid, '____________________________________   character*40 comment              \n');
+        fprintf(fid, '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n');  
+        fprintf(fid, '\n');
+        fclose(fid);
+        
+        %WRITE pathnames
+        if strcmp(type, 'methyliodide')
+            fid_1 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Methyliodide/pathnames-' istr], 'wt');
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Methyliodide_%s/options/options-%i/\n', simulation, j);        
+            fprintf(fid_1, './M91/Methyliodide_%s/outputs-%i/\n', simulation, j);        
+            fprintf(fid_1, '\n');        
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Methyliodide_%s/AVAILABLE\n', simulation);        
+            fprintf(fid_1, '============================================\n'); 
+            fprintf(fid_1, '\n');
+            fclose(fid_1);
+        
+        elseif strcmp(type, 'dibromomethane')
+            fid_1 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Dibromomethane/pathnames-' istr], 'wt');
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Dibromomethane_%s/options/options-%i/\n', simulation, j);        
+            fprintf(fid_1, './M91/Dibromomethane_%s/outputs-%i/\n', simulation, j);        
+            fprintf(fid_1, '\n');        
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Dibromomethane_%s/AVAILABLE\n', simulation);        
+            fprintf(fid_1, '============================================\n'); 
+            fprintf(fid_1, '\n');
+            fclose(fid_1);
+        elseif strcmp(type, 'bromoform')
+            fid_1 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Bromoform/pathnames-' istr], 'wt');
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Bromoform_%s/options/options-%i/\n', simulation, j);        
+            fprintf(fid_1, './M91/Bromoform_%s/outputs-%i/\n', simulation, j);        
+            fprintf(fid_1, '\n');        
+            fprintf(fid_1, '/usit/abel/u1/sarahgj/Flexpart/Runs/M91/Bromoform_%s/AVAILABLE\n', simulation);        
+            fprintf(fid_1, '============================================\n'); 
+            fprintf(fid_1, '\n');            
+            fclose(fid_1);                                               
+        else
+            fprintf('Thats wierd :O')
+        end
+        
+        %WRITE COMMAND
+        if strcmp(type, 'methyliodide')
+           fid_2 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Methyliodide/COMMAND-' istr], 'wt');
+        elseif strcmp(type, 'dibromomethane')
+           fid_2 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Dibromomethane/COMMAND-' istr], 'wt');
+        elseif strcmp(type, 'bromoform')
+           fid_2 = fopen(['/uio/hume/student-u17/sarahgj/Master/Data/Flexpart_files/M91/Bromoform/COMMAND-' istr], 'wt');
+        else
+            fprintf('Thats wierd :O')
+        end
+
+        %write to files
+        fprintf(fid_2, '********************************************************************************\n');
+        fprintf(fid_2, '*                                                                              *\n');
+        fprintf(fid_2, '*      Input file for the Lagrangian particle dispersion model FLEXPART        *\n');
+        fprintf(fid_2, '*                           Please select your options                         *\n');
+        fprintf(fid_2, '*                                                                              *\n');
+        fprintf(fid_2, '********************************************************************************\n');
+        fprintf(fid_2, '\n');
+        fprintf(fid_2, '1               LDIRECT           1 FOR FORWARD SIMULATION, -1 FOR BACKWARD SIMULATION\n');      
+        fprintf(fid_2, '%s %s YYYYMMDD HHMISS   BEGINNING DATE OF SIMULATION\n', startstring(i,1:8), startstring(i,10:15)); 
+        fprintf(fid_2, '%s %s YYYYMMDD HHMISS   ENDING DATE OF SIMULATION\n', end_simulation_string(i,1:8), end_simulation_string(i,10:15)); 
+        fprintf(fid_2, '10800           SSSSS             OUTPUT EVERY SSSSS SECONDS \n');
+        if strcmp(type, 'methyliodide')
+            fprintf(fid_2, '1800            SSSSS             TIME AVERAGE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '900             SSSSS             SAMPLING RATE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '999999999       SSSSSSS           TIME CONSTANT FOR PARTICLE SPLITTING (IN SECONDS) \n');
+            fprintf(fid_2, '900             SSSSS             SYNCHRONISATION INTERVAL OF FLEXPART (IN SECONDS) \n');
+        elseif strcmp(type, 'bromoform')
+            fprintf(fid_2, '3600            SSSSS             TIME AVERAGE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '1800            SSSSS             SAMPLING RATE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '999999999       SSSSSSS           TIME CONSTANT FOR PARTICLE SPLITTING (IN SECONDS) \n');
+            fprintf(fid_2, '1800            SSSSS             SYNCHRONISATION INTERVAL OF FLEXPART (IN SECONDS) \n');
+        elseif strcmp(type, 'dibromomethane')
+            fprintf(fid_2, '7200            SSSSS             TIME AVERAGE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '3600            SSSSS             SAMPLING RATE OF OUTPUT (IN SSSSS SECONDS) \n');
+            fprintf(fid_2, '999999999       SSSSSSS           TIME CONSTANT FOR PARTICLE SPLITTING (IN SECONDS) \n');
+            fprintf(fid_2, '3600            SSSSS             SYNCHRONISATION INTERVAL OF FLEXPART (IN SECONDS) \n');
+        end
+        fprintf(fid_2, '-5.0            CTL               FACTOR, BY WHICH TIME STEP MUST BE SMALLER THAN TL \n');
+        fprintf(fid_2, '4               IFINE             DECREASE OF TIME STEP FOR VERTICAL MOTION BY FACTOR IFINE \n');
+        fprintf(fid_2, '1               IOUT              1 CONC. (RESID. TIME FOR BACKWARD RUNS) OUTPUT,2 MIX. RATIO OUTPUT,3 BOTH,4 PLUME TRAJECT.,5=1+4 \n');
+        fprintf(fid_2, '1               IPOUT             PARTICLE DUMP: 0 NO, 1 EVERY OUTPUT INTERVAL, 2 ONLY AT END \n');
+        fprintf(fid_2, '1               LSUBGRID          SUBGRID TERRAIN EFFECT PARAMETERIZATION: 1 YES, 0 NO \n');
+        fprintf(fid_2, '1               LCONVECTION       CONVECTION: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               LAGESPECTRA       AGE SPECTRA: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               IPIN              CONTINUE SIMULATION WITH DUMPED PARTICLE DATA: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               IOUTPUTFOREACHREL CREATE AN OUPUT FILE FOR EACH RELEASE LOCATION: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               IFLUX             CALCULATE FLUXES: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               MDOMAINFILL       DOMAIN-FILLING TRAJECTORY OPTION: 1 YES, 0 NO \n');
+        fprintf(fid_2, '1               IND_SOURCE        1=MASS UNIT , 2=MASS MIXING RATIO UNIT \n');
+        fprintf(fid_2, '1               IND_RECEPTOR      1=MASS UNIT , 2=MASS MIXING RATIO UNIT \n');
+        fprintf(fid_2, '0               MQUASILAG         QUASILAGRANGIAN MODE TO TRACK INDIVIDUAL PARTICLES: 1 YES, 0 NO \n');
+        fprintf(fid_2, '0               NESTED_OUTPUT     SHALL NESTED OUTPUT BE USED? YES, 0 NO \n');
+        fprintf(fid_2, '2               LINIT_COND        INITIAL COND. FOR BW RUNS: 0=NO,1=MASS UNIT,2=MASS MIXING RATIO UNIT\n');
+        fprintf(fid_2, '0               SURF_ONLY         IF THIS IS SET TO 1, OUTPUT IS WRITTEN ONLY OUT FOR LOWEST LAYER                                                                         \n');
+        fclose(fid_2);        
+    end
+end
+
+
+
